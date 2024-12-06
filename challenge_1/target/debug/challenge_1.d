057b8e09a5143102dbda5b8257e73d44c96582c2
Chris Bakopoulos <christian.bakopoulos@gmail.com>
2024-12-06 11:32:11 -0500

    cleanup repo

--- challenge_1/target/debug/challenge_1.d
+++
@@ -1,1 +0,0 @@
-/home/chrisb/code/advent_2024/challenge_1_part_1/target/debug/challenge_1: /home/chrisb/code/advent_2024/challenge_1_part_1/src/main.rs
--- challenge_1/target/debug/deps/challenge_1-81a99ca362c9091c.d
+++
@@ -1,5 +0,0 @@
-/home/chrisb/code/advent_2024/challenge_1_part_1/target/debug/deps/challenge_1-81a99ca362c9091c: src/main.rs
-
-/home/chrisb/code/advent_2024/challenge_1_part_1/target/debug/deps/challenge_1-81a99ca362c9091c.d: src/main.rs
-
-src/main.rs:
